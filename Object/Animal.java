211f04c54595d7e22f456201b2ff239121be78e6
goeun <78770239+cge1111@users.noreply.github.com>
2021-04-06 12:24:57 +0900

    Add files via upload

---
+++ Object/Animal.java
@@ -0,0 +1,10 @@
+package Object;
+
+public class Animal {
+    String name;
+    public static void main(String args[]) {
+        
+        Animal cat = new Animal();
+        System.out.println(cat.name);
+    }
+}
