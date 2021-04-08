a5ae99c47f1bdfaae15b55ca30482ab67aa13d57
goeun <78770239+cge1111@users.noreply.github.com>
2021-04-08 13:04:01 +0900

    Add files via upload

---
+++ input/StreamTest.java
@@ -0,0 +1,15 @@
+package input;
+
+import java.util.Scanner;  // 파이썬 = input
+
+public class StreamTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // 객체입력
+
+        System.out.println(sc.next());  
+        // next - 단어
+        // nextLine - 라인
+        // nextInt - 정수
+    }
+}
+
